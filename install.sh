ad831f1a50b100b75fb0171dab3b3c02a0a69dc9
Emil Nilsson <eonilsson@gmail.com>
2016-03-28 18:24:33 +0200

    Generate .tmux.conf from a base and version specific file

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
 DIR=$(cd "$(dirname $0)"; pwd)
-FILES=".tmux.conf"
+TMUX_VERSION=$(tmux -V | cut -d ' ' -f2)
+CONFIG=".tmux.conf"
+BASE_CONFIG=".tmux.base.conf"
+VERSION_CONFIG=".tmux.$TMUX_VERSION.conf"
+FILES="$CONFIG"
+
+# build .tmux.conf by including any version specific files
+cat $DIR/$BASE_CONFIG > $DIR/$CONFIG
+if [ -f "$DIR/$VERSION_CONFIG" ]; then
+	cat $DIR/$VERSION_CONFIG >> $DIR/$CONFIG
+fi
+
 for file in $FILES; do
 	 ln -siFn $DIR/$file $HOME/$file
 done
